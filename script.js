07fc610adc9f816fc9ccc62cd5a51322697dabd7
Clemoooo <103392031+ClemooooFx@users.noreply.github.com>
2025-09-19 13:03:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -270,7 +270,7 @@ function processTBillData(jsonData, chartId, tableId) {
         };
     });
 
-    createChartWithNavigator(chartId, 't-bills-navigator', dates, datasets);
+    renderMultiLineChart(chartId, dates, datasets);
     const headers = ['Issue Date', 'Tenor', 'Weighted Average Rate'];
     renderTable(tableId, headers, jsonData);
 }
