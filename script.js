665aa55010d08de37d2194578dd8e11c9792c3af
Clemoooo <103392031+ClemooooFx@users.noreply.github.com>
2025-09-20 19:02:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -358,7 +358,7 @@ function processCBRData(jsonData, chartId, tableId) {
         pointRadius: 0
     }];
 
-    renderCBRChart(chartId, dates, datasets);
+    renderMultiLineChart(chartId, dates, datasets);
     const headers = ['Date', 'Rate'];
     renderTable(tableId, headers, jsonData);
 }
