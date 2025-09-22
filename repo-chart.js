4fa72dc4096c6b29f47c740cfce878953a360195
Clemoooo <103392031+ClemooooFx@users.noreply.github.com>
2025-09-22 12:48:44 +0300

    Update repo-chart.js

--- repo-chart.js
+++ repo-chart.js
@@ -6,6 +6,37 @@ document.addEventListener('DOMContentLoaded', () => {
     loadAndDisplay('data/Repo and Reverse Repo.xlsx', 'repo-chart', 'repo-table');
 });
 
+function loadAndDisplay(filePath, chartId, tableId) {
+    fetch(filePath)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            return res.arrayBuffer();
+        })
+        .then(ab => {
+            const workbook = XLSX.read(ab, { type: "array" });
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+
+            if (filePath.includes('Treasury Bills Average Rates.xlsx')) {
+                processTBillData(worksheet, chartId, tableId);
+            } else if (filePath.includes('Issues of Treasury Bonds.xlsx')) {
+                processTBondData(worksheet, chartId, tableId);
+            } else if (filePath.includes('Central Bank Rate (CBR).xlsx')) {
+                const sortedData = worksheet.sort((a, b) => {
+                    const dateA = new Date(a['Date'].split('/').reverse().join('-'));
+                    const dateB = new Date(b['Date'].split('/').reverse().join('-'));
+                    return dateA - dateB;
+                });
+                processCBRData(sortedData, chartId, tableId);
+            } else if (filePath.includes('Repo and Reverse Repo.xlsx')) {
+                processRepoData(worksheet, chartId, tableId);
+            }
+        })
+        .catch(error => console.error(`Failed to load or parse the file at ${filePath}:`, error));
+}
+
 function processRepoData(jsonData, chartId, tableId) {
     // Filter out rows where the "Date" is blank
     const filteredData = jsonData.filter(row => row['Date']);
