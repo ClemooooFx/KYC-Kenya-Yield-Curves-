746bbef6e0a4759d1f59a03f1782d1aa129a4c2c
Clemoooo <103392031+ClemooooFx@users.noreply.github.com>
2025-09-27 14:56:00 +0300

    Update inflation.js

--- inflation.js
+++ inflation.js
@@ -32,18 +32,18 @@ function loadExcelFile(filePath) {
 
 function getMonthNumber(monthName) {
     const months = {
-        'January': 1, 'jan': 1,
-        'February': 2, 'feb': 2,
-        'March': 3, 'mar': 3,
-        'April': 4, 'apr': 4,
-        'May': 5,
-        'June': 6, 'jun': 6,
-        'July': 7, 'jul': 7,
-        'August': 8, 'aug': 8,
-        'September': 9, 'sep': 9, 'sept': 9,
-        'October': 10, 'oct': 10,
-        'November': 11, 'nov': 11,
-        'December': 12, 'dec': 12
+        'january': 1, 'jan': 1,
+        'february': 2, 'feb': 2,
+        'march': 3, 'mar': 3,
+        'april': 4, 'apr': 4,
+        'may': 5,
+        'june': 6, 'jun': 6,
+        'july': 7, 'jul': 7,
+        'august': 8, 'aug': 8,
+        'september': 9, 'sep': 9, 'sept': 9,
+        'october': 10, 'oct': 10,
+        'november': 11, 'nov': 11,
+        'december': 12, 'dec': 12
     };
     
     if (!monthName) return null;
